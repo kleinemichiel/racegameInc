c40c88326bbcd849bde8d7499f98278db7243fcc
wfikkert <wfikkert@gmail.com>
2015-12-07 15:20:53 +0100

    MainMenuSP
    MainMenuSP:
    Title in center of the screen.
    GoTo gameloop

--- MainProject/MainMenu/MainMenu/MainMenuSP.cpp
+++ MainProject/MainMenu/MainMenu/MainMenuSP.cpp
@@ -7,7 +7,6 @@
 
 #include "MainMenuSP.h"
 #include "SettingsMenu.h"
-#include "highscoreMenu.h"
 
 
 uint8_t setValue = 0;
@@ -17,19 +16,19 @@ extern void showMainMenu(MI0283QT9 lcd, MENUOBJECTS obj, MY_USART serial){
 	lcd.fillScreen(BACKGROUND);
 	while(1){
 		
-		char title[] = "Settings";
+		char title[] = "RaceGame";
 		uint8_t lengthOfString = (strlen(title) * 26); 
 		uint8_t x = (240 - lengthOfString);
 		
-		lcd.drawText(x, 15, "RaceGameInc", OBJECTCOLOR, BACKGROUND, 3);
+		lcd.drawText(x, 15, title[], OBJECTCOLOR, BACKGROUND, 3);
 		if(obj.drawButton(lcd, "Singleplayer", 20, 75, 200, 40)){
-			
+			showGame(lcd,serial);
 		}
 		if(obj.drawButton(lcd, "Settings", 20, 135, 200, 40)){
 			showSettingMenu(lcd,obj,serial);
 		}
 		if(obj.drawButton(lcd, "Highscore", 20, 195, 200, 40)){
-			showHighscoreMenu(lcd,obj,serial);
+			showHighscoreMenu(lcd, obj, serial);
 		}
 		
 	}
