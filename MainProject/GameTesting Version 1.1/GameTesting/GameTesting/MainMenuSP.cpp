aa1f405e9f86cdd67f52aafaf08446923805f20d
EldinZenderink <s1077709@student.windesheim.nl>
2015-12-15 16:02:52 +0100

    MainMenu: player
    Player visible in main menu

--- MainProject/GameTesting Version 1.1/GameTesting/GameTesting/MainMenuSP.cpp
+++ MainProject/GameTesting Version 1.1/GameTesting/GameTesting/MainMenuSP.cpp
@@ -21,15 +21,26 @@ extern void showMainMenu(MI0283QT9 lcd, MENUOBJECTS obj, MY_USART serial){
 		uint8_t x = (240 - lengthOfString);
 		
 		lcd.drawText(x, 15, title, OBJECTCOLOR, BACKGROUND, 3);
-		if(obj.drawButton(lcd, "Singleplayer", 20, 75, 200, 40)){
+		if(obj.drawButton(lcd, "Singleplayer", 20, 55, 200, 40)){
 			showGame(lcd, obj, serial);
 		}
-		if(obj.drawButton(lcd, "Settings", 20, 135, 200, 40)){
+		if(obj.drawButton(lcd, "Settings", 20, 110, 200, 40)){
 			showSettingMenu(lcd,obj,serial);
 		}
-		if(obj.drawButton(lcd, "Highscore", 20, 195, 200, 40)){
+		if(obj.drawButton(lcd, "Highscore", 20, 165, 200, 40)){
 			showHighscoreMenu(lcd, obj, serial);
 		}
+		lcd.drawRect(100, 235, 40, 64, RGB(red,green,blue));
+		lcd.drawLine(100+1,235+16,100+10, 235+26, RGB(red,green,blue));
+		lcd.drawLine(100+39,235+16,100+30, 235+26, RGB(red,green,blue));
+		lcd.drawLine(100+10,235+26,100+30, 235+26, RGB(red,green,blue));
+		lcd.drawLine(100+10,235+38,100+30, 235+38, RGB(red,green,blue));
+		lcd.drawLine(100+1,235+48,100+10, 235+38, RGB(red,green,blue));
+		lcd.drawLine(100+39,235+48,100+30, 235+38, RGB(red,green,blue));
+		lcd.drawLine(100+10,235+26,100+10, 235+38, RGB(red,green,blue));
+		lcd.drawLine(100+30,235+26,100+30, 235+38, RGB(red,green,blue));
+		lcd.drawLine(100,235+16,100+38, 235+16, RGB(red,green,blue));
+		lcd.drawLine(100,235+48,100+38, 235+48, RGB(red,green,blue));
 		
 	}
 }
