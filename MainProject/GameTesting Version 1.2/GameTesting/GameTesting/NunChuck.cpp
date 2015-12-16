7288fa8954266968d4acbd7974c89b10b503164f
EldinZenderink <s1077709@student.windesheim.nl>
2015-12-16 21:29:45 +0100

    v1.2: Memory Optimizing
    NEW:
    -includeMe:
    Now all the libraries should be included here, so in all other libraries
    it will be automaticly included as well(instead of doing it manually),
    you need to include this header file in your library header to keep
    everything working!
    
    CHANGES:
    -menuObjects library no longer works like a class but uses extern
    functions instead
    
    -all the other libraries:
    -removed all includes in the header file for each library and added
    these to includeMe
    -removed MENUOBJECTS and MY_USART in all menu functions and passthrough
    in gameloop
    -included includeMe in all the header files
    -added pragma once to all header files
    
    Result:
    going from 94 % usage to 86% usage
    
    #! NOTE THAT THIS HAS ONLY TESTED WHITH COMPILER, ACTUAL USE HAS NOT
    BEEN TESTED YET !#

---
+++ MainProject/GameTesting Version 1.2/GameTesting/GameTesting/NunChuck.cpp
@@ -0,0 +1,141 @@
+/*
+ * NunChuck.cpp
+ *
+ * Created: 21-11-2015 11:10:22
+ *  Author: Eldin
+ * Make sure Arduino.h & Wire.h is included in your main program and you ran the init() method before initialising the nunchuck!!!
+ */ 
+
+
+
+
+
+#include "NunChuck.h"
+
+//data container
+char NUNCHUCK::nunDataArr[6];
+
+//starts handshake with nunchuck
+uint8_t NUNCHUCK::nunchuck_init(){
+	Wire.begin();
+	Wire.beginTransmission(NUNADDRESS);
+	Wire.write(0x40);
+	Wire.write(DATARQ);
+	if(Wire.endTransmission() == 0){
+		return 1;
+	} else {
+		return 0;
+	}
+	
+}
+
+//requests data from the nunchuck
+uint8_t NUNCHUCK::request_data(){
+	Wire.beginTransmission(NUNADDRESS);
+	Wire.write(DATARQ);
+	if(Wire.endTransmission() == 0){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+//converts data to useable values
+uint16_t NUNCHUCK::decode_data(uint16_t data){
+	
+	return (data ^ 0x17) + 0x17;
+}
+
+//receives / retreives the data and puts it in the container
+uint8_t NUNCHUCK::retreive_data(){
+	
+	Wire.requestFrom(NUNADDRESS, 6);
+	
+	uint8_t index = 0;
+	
+	while(Wire.available()){
+		nunDataArr[index] = decode_data(Wire.read());
+		index++;
+	}
+	
+	
+	
+	request_data();
+	
+	if(nunDataArr[5] != NULL){
+		return 1;
+	} else {
+		return 0;
+	}
+	
+	
+}
+
+//returns the position of the joy stick on the x axle
+uint8_t NUNCHUCK::getJoyX(){
+	if(nunDataArr[0] != NULL){
+		return nunDataArr[0];
+	} else {
+		return 0;
+	}
+}
+
+//returns the position of the joy stick on the y axle
+uint8_t NUNCHUCK::getJoyY(){
+	if(nunDataArr[1] != NULL){
+		return nunDataArr[1];
+		} else {
+		return 0;
+	}
+}
+
+//returns the position of the accellerometer on the x axle as a 10 bit value
+uint8_t NUNCHUCK::getAccX(){
+	if(nunDataArr[2] != NULL){
+		uint8_t first8bitsX = (nunDataArr[2]);
+		return first8bitsX;
+	} else {
+		return 0;
+	}
+}
+
+//returns the position of the accellerometer on the y axle as a 10 bit value
+uint8_t NUNCHUCK::getAccY(){
+	if(nunDataArr[3] != NULL){
+		uint8_t first8bitsY = (nunDataArr[3]);
+		return first8bitsY;
+	} else {
+		return 0;
+	}
+}
+
+//returns the position of the accellerometer on the z axle as a 10 bit value
+uint8_t NUNCHUCK::getAccZ(){
+	if(nunDataArr[4] != NULL){
+		uint8_t first8bitsZ = (nunDataArr[4]);
+		return first8bitsZ;
+	} else {
+		return 0;
+	}
+}
+
+//returns true or false where true is button c pressed
+uint8_t NUNCHUCK::getC(){
+	if(nunDataArr[5] != NULL){
+		return (((nunDataArr[5] & 0x02) >> 1) ^ 0x01) ;
+	} else {
+		return 0;
+	}
+}
+
+//returns true or false where true is button z pressed
+uint8_t NUNCHUCK::getZ(){
+	if(nunDataArr[5] != NULL){
+		return ((nunDataArr[5] & 0x01) ^ 0x01);
+	} else {
+		return 0;
+	}
+}
+
+
+
---
+++ MainProject/GameTesting Version 1.2/GameTesting/GameTesting/includeMe.h
@@ -0,0 +1,37 @@
+/*
+ * includeMe.h
+ *
+ * Created: 16-12-2015 17:33:09
+ *  Author: Eldin
+ */ 
+
+#pragma once
+
+#include <EEPROM.h>
+#include "stdint.h"
+#include "digitalWriteFast.h"
+#include "GraphicsLib.h"
+#include "MI0283QT9.h"
+#include "USART.h"
+#include "NunChuck.h"
+#include "StateAndActions.h"
+#include "menuObjects.h"
+#include "MainMenuSP.h"
+#include "SettingsMenu.h"
+#include "highscoreMenu.h"
+#include "PauseMenu.h"
+#include "GameOverMenu.h"
+#include "Default_Layout.h"
+#include "GridLayout.h"
+#include "GameObjects.h"
+#include "GameLoop.h"
+
+/*
+#ifndef INCLUDEME_H_
+#define INCLUDEME_H_
+
+
+
+
+
+#endif / * INCLUDEME_H_ * /*/
