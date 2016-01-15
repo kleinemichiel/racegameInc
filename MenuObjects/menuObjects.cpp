e77d71ff15b19c011930b6363f93b4201d2ac5fe
wfikkert <wfikkert@gmail.com>
2016-01-15 14:19:42 +0100

    MenuObjects Buttons
    MADE BY DANIEK
    Buttons won't be drawn on the screen every time the loop runs

--- MenuObjects/menuObjects.cpp
+++ MenuObjects/menuObjects.cpp
@@ -5,40 +5,28 @@
  * Created: 26-11-2015 16:58:51
  *  Author: Eldin
  */ 
-
 #include "menuObjects.h"
-
 //creates buttons with somewhat visual feedback when touched, returns 1 or 0 depending on press
 uint8_t drawButton(MI0283QT9 lcd, char text[], uint16_t x, uint16_t y, uint16_t w, uint16_t h){
 	
 	uint16_t textWidth = (8 * strlen(text));
+	uint8_t returnvalue = 0;
 	
 	lcd.touchRead();
+	
+	lcd.drawRect(x, y,w,h, OBJECTCOLOR);
+	lcd.drawText(x + ((w / 2) - textWidth), (y + (h / 2) - 8) , text, OBJECTCOLOR, BACKGROUND, 2);
 	
 	if(lcd.touchZ() && lcd.touchX() >= x && lcd.touchX() <= (x + w) && lcd.touchY() >= y && lcd.touchY() <= (y + h)){
 	
 		lcd.fillRect(x, y,w,h, OBJECTCOLOR);
 		lcd.drawText(x + ((w / 2) - textWidth), (y + (h / 2) - 8) , text, BACKGROUND, OBJECTCOLOR, 2);
 		
-		_delay_ms(500);
-		
-		lcd.fillRect(x, y,w,h, BACKGROUND);
-		
-		return 1;
-	
-	} else {
-		
-		lcd.drawRect(x, y,w,h, OBJECTCOLOR);
-		lcd.drawText(x + ((w / 2) - textWidth), (y + (h / 2) - 8) , text, OBJECTCOLOR, BACKGROUND, 2);
-		
-		return 0;
+		returnvalue = 1;
 		
 	} 
-		
+		return returnvalue;
 }
-
-
-
 //creates a slider. If you click on the slider it will create a new fillrect on the position you touched.
 uint8_t drawSlider(MI0283QT9 lcd, uint16_t y, uint8_t sValue, uint8_t sRed, uint8_t sGreen, uint8_t sBlue){
 	
@@ -59,8 +47,6 @@ uint8_t drawSlider(MI0283QT9 lcd, uint16_t y, uint8_t sValue, uint8_t sRed, uint
 	
 	return sliderValue;
 }
-
-
 //creates checkboxes which need a pointer to a int acting as a bool. The value of the pointer changes when you press the checkbox. If the value changes to 1 then the checkbox will be 'selected' and if the value changes to 0 then the checkbox will be 'not selected'.
 void drawCheckbox(MI0283QT9 lcd, uint16_t x, uint16_t y, uint16_t s, uint8_t *setValue)
 {
@@ -89,7 +75,6 @@ void drawCheckbox(MI0283QT9 lcd, uint16_t x, uint16_t y, uint16_t s, uint8_t *se
 		lcd.drawRect(x, y,s,s, OBJECTCOLOR);
 	}
 }
-
 //function for center the text, you are able to center the text for every size you can choose.
 uint8_t centerText(char text[], uint8_t size){
 	if(size == 2){
@@ -105,11 +90,9 @@ uint8_t centerText(char text[], uint8_t size){
 		return 0;
 	}
 }	
-
 	
 //Creates a button. If you click on the button the next letter will be printed in the button.
 char buttonChar[] = "A";
-
 char drawCharSelector(MI0283QT9 lcd, uint16_t x, uint16_t y, char previous){
 	
 	buttonChar[0] = previous;
